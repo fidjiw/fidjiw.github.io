6ac0be7cf040a22e7e66c1c49c9707b3ae9f2bf1
xfeng <49848047+fidjiw@users.noreply.github.com>
2024-08-10 21:14:14 +0800

    123

---
+++ notion-hugo.config.ts
@@ -0,0 +1,30 @@
+import { UserConfig } from "./src/config"
+
+const userConfig: UserConfig = {
+    mount: {
+        manual: false,
+        page_url: 'https://fidjiw.notion.site/Notion-DoIt-3fd347f96ef040b2a1957de621ca0253',
+        pages: [
+            // {
+            //     page_id: '<page_id>',
+            //     target_folder: 'path/relative/to/content/folder'
+            // }
+            {
+                page_id: '45eb121158b9489480ec000fd25c812b',
+                target_folder: '.'
+            }
+        ],
+        databases: [
+            // {
+            //     database_id: '<database_id>',
+            //     target_folder: 'path/relative/to/content/folder'
+            // }
+            {
+                database_id: 'b7b1816c05ec464391c8c111fa242985',
+                target_folder: '.'
+            }
+        ],
+    }
+}
+
+export default userConfig;
